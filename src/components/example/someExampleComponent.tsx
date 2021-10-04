3a50c99ea4a051726f1989a487ce422387c14880
Alex Toro <alextoro@Ohjelmointi-03-MacBook-Pro.local>
2021-10-04 20:22:49 +0900

    korjaus

---
+++ src/components/example/someExampleComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './someExampleComponent.module.scss'
+
+type SomeExampleComponentProps = {
+
+}
+
+const SomeExampleComponent: React.FC<SomeExampleComponentProps> = () => {
+  return (
+    <div>
+      SomeExampleComponent
+    </div>
+  )
+}
+
+export default SomeExampleComponent
