ac2f418b92fd057e42322ab8a3d3d64586c841f7
alex <alextoro306@gmail.com>
2022-02-27 18:44:31 +0200

    viimeset muutokset esim procjets ja somet

--- src/components/sites/projects/projects.tsx
+++ src/components/sites/projects/projects.tsx
@@ -8,21 +8,25 @@ type ProjectsProps = {
 const Projects: React.FC<ProjectsProps> = () => {
   return (
     <div className={styles.links}>
-      <a className={styles.gitHubLink} href="https://github.com/alextoro306" target="_blank">
-      Open In GitHub
-      </a>
-      <a className={styles.gamesLink} href="https://github.com/alextoro306/portfolio" target="_blank">
-      Open In Unity
-      </a>
-      <a className={styles.portfolioLink} href="https://play.unity.com/u/lexaaaaa" target="_blank">
-      Open In GitHub
-      </a>
+        <a className={styles.gitHubLink} href="https://github.com/alextoro306" target="_blank">
+        Open In GitHub
+        </a>
+        <div className={styles.links2}>
+          <a className={styles.portfolioLink} href="https://github.com/alextoro306/portfolio" target="_blank">
+          Open In Unity
+          </a>
+        </div>
+        <div className={styles.links3}>
+          <a className={styles.gamesLink} href="https://play.unity.com/u/lexaaaaa" target="_blank">
+          Open In GitHub
+          </a>
+        </div>
         <div className={styles.container}>
           <h2>👤GitHub Profile</h2>
           <h3>🎮Games</h3>
           <h4>📁Portfolio</h4>
         </div>
-    </div>
+        </div>
   )
 }
 
