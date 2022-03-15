9d9b646ee230413ea9a332bd471016f6f73e71cc
alex <alextoro306@gmail.com>
2022-03-15 09:19:22 +0200

    rel muutokset

--- src/components/sites/projects/projects.tsx
+++ src/components/sites/projects/projects.tsx
@@ -10,15 +10,15 @@ const Projects: React.FC<ProjectsProps> = () => {
     <div className={styles.links}>
         <div className={styles.linkCard1}>
         <span><p>👤 GitHub Profile</p></span>
-        <a href="https://github.com/alextoro306" target="_blank">Open In GitHub</a>
+        <a href="https://github.com/alextoro306" target="_blank" rel="noreferrer">Open In GitHub</a>
         </div>
         <div className={styles.linkCard2}>
         <span><p>🎮 Games</p></span>
-        <a href="https://play.unity.com/u/lexaaaaa" target="_blank">Open In Unity</a>
+        <a href="https://play.unity.com/u/lexaaaaa" target="_blank" rel="noreferrer">Open In Unity</a>
         </div>
         <div className={styles.linkCard3}>
         <span><p>📁 Portfolio</p></span>
-        <a href="https://github.com/alextoro306/portfolio" target="_blank">Open In GitHub</a>
+        <a href="https://github.com/alextoro306/portfolio" target="_blank" rel="noreferrer">Open In GitHub</a>
         </div>
     </div>
   )
