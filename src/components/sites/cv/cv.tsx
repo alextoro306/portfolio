1c9dc5217ce82e3e4a1cc74d4a12c2b404ce5ec7
alex <alextoro306@gmail.com>
2022-03-16 20:22:09 +0200

    oma cv

--- src/components/sites/cv/cv.tsx
+++ src/components/sites/cv/cv.tsx
@@ -12,179 +12,120 @@ const Cv: React.FC<CvProps> = () => {
     <div className={useDarkmode ? styles.dark : styles.light}>
 
 <div className={styles.container}>
-  <h1>CV</h1>
-  <h2>Hi, My name is Alex Toro and im studying information and communication technologies. Im currently studying at Varia Myyrmäki Vantaa. The things i've learnt are the following: Basics of Java, JavaScript, TypeScript, React, CSS and SCSSI've studied the basics of Unity, and C# scripting for Unity. I have been studying for a little over a year and a half so far.</h2>
-  <h3></h3>
-  <h4>Contact</h4>
-  <h5>alextoro306@gmail.com</h5>
-  <h6>alextoro306@gmail.com</h6>
-  <a>+358442412749</a>
-  <b>Espoo, Finland</b>
-</div>
 
-{/* <div className={styles.resume}>
-   <div className={styles.resume_left}>
-     <div className={styles.resume_profile}>
-       <img src="https://images.news18.com/ibnlive/uploads/2020/05/1590926604_anonymous-minneapolis-pd-video-message-facebook-may-29.jpg?im=FitAndFill,width=1200,height=675"alt="profile_pic"/>
-     </div>
-     <div className={styles.resume_content}>
-       <div className={`${styles.class1resume_item} ${styles.class2resume_info}`}>
-         <div className={styles.title}>
-           <p className={styles.bold}>Alex Toro</p>
-         </div>
-         <ul>
-           <li>
-             <div className={styles.regular}>
-                Developer
-             </div>
-           </li>
-           <li>
-             <div className={styles.data}>
-               Espoo, Finland
-             </div>
-           </li>
-           <li>
-             <div className={styles.data}>
-               +358442412749
-             </div>
-           </li>
-           <li>
-             <div className={styles.data}>
-               alextoro306@gmail.com
-             </div>
-           </li>
-           <li>
-           </li>
-         </ul>
-       </div>
-       <div className={`${styles.class1resume_item} ${styles.class2resume_skills}`}>
-         <div className={styles.title}>
-           <p className={styles.bold}>skill's</p>
-         </div>
-         <ul>
-           <li>
-             <div className={styles.skill_name}>
-               HTML
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-           <li>
-             <div className={styles.skill_name}>
-               CSS
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-           <li>
-             <div className={styles.skill_name}>
-               React
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-           <li>
-             <div className={styles.skill_name}>
-               Unity
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-           <li>
-             <div className={styles.skill_name}>
-               TypeScript
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-           <li>
-             <div className={styles.skill_name}>
-               JavaScript
-             </div>
-             <div className={styles.skill_progress}>
-               <span style={{width: "50%"}}></span>
-             </div>
-             <div className={styles.skill_per}>[Advanced]</div>
-           </li>
-         </ul>
-       </div>
-     </div>
+  <div className={styles.tag1}>
+    <span><p>CV</p></span>
   </div>
-  <div className={styles.resume_right}>
-    <div className={`${styles.class1resume_item} ${styles.class2resume_about}`}>
-        <div className={styles.title}>
-           <p className={styles.bold}>About Me</p>
-         </div>
-        <p>Hi, My name is Alex and im studying information and communication technologies. Im currently studying at Varia Myyrmäki Vantaa. The things i've learnt are the following: Basics of Java, JavaScript, TypeScript, React, CSS and SCSSI've studied the basics of Unity, and C# scripting for Unity. I have been studying for a little over a year and a half so far.</p>
-    </div>
-    <div className={`${styles.class1resume_item} ${styles.class2resume_work}`}>
-        <div className={styles.title}>
-           <p className={styles.bold}>Work Experience</p>
-         </div>
-        <ul>
-            <li>
-                <div className={styles.date}>2018</div> 
-                <div className={styles.info}>
-                     <p className="semi-bold">Kindergarten</p> 
-                  <p>I worked at kindergarten when i was 14 years old.</p>
-                </div>
-            </li>
-            <li>
-              <div className={styles.date}>2019</div>
-              <div className={styles.info}>
-                     <p className="semi-bold">Food Restaurant</p> 
-                  <p>I worked at restaurant when i was 15 years old.</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div className={`${styles.class1resume_item} ${styles.class2resume_education}`}>
-      <div className={styles.title}>
-           <p className={styles.bold}>Education</p>
-         </div>
-      <ul>
-            <li>
-                <div className={styles.date}>2011 - 2017</div> 
-                <div className={styles.info}>
-                     <p className="semi-bold">Primary School</p> 
-                  <p>I was in three different schools. They were Koulumestarin koulu (2011-2014), Ymmerstan koulu (2014-2016) and Juvanpuiston koulu (2016-2017).
-                  </p>
-                </div>
-            </li>
-            <li>
-              <div className={styles.date}>2017 - 2020</div>
-              <div className={styles.info}>
-                     <p className="semi-bold">Junior High School</p> 
-                  <p>I studied the whole junior high school time at Juvanpuiston koulu.</p>
-                </div>
-            </li>
-            <li>
-              <div className={styles.date}>2020 - </div>
-              <div className={styles.info}>
-                     <p className="semi-bold">Vocational School</p> 
-                  <p>I study at Varia Myyrmäki.</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div className={`${styles.class1resume_item} ${styles.class2resume_hobby}`}>
-      <div className={styles.title}>
-           <p className={styles.bold}>Hobby</p>
-           <p>I play video games and hang out with my friends.</p>
-         </div>
-    </div>
+
+  <div className={styles.tag2}>
+    <span><p>Hi, My name is Alex Toro and im studying information and communication technologies. Im currently studying at Varia Myyrmäki Vantaa. The things i've learnt are the following: Basics of Java, JavaScript, TypeScript, React, CSS and SCSSI've studied the basics of Unity, and C# scripting for Unity. I have been studying for a little over a year and a half so far.</p></span>
   </div>
-</div> */}
+
+  <div className={styles.tag3}>
+    <span><p></p></span>
+  </div>
+  
+  <div className={styles.tag4}>
+    <span><p>Contact</p></span>
+  </div>
+
+  <div className={styles.tag5}>
+  <span> <p>📩 alextoro306@gmail.com</p> <p>📞 +358442412749</p> <p>📍 Espoo, Finland</p> </span>
+  </div>
+
+  <div className={styles.tag6}>
+  <span><p></p></span>
+  </div>
+
+  <div className={styles.tag7}>
+    <span><p>Education</p></span>
+  </div>
+
+  <div className={styles.tag8}>
+    <span><p>2011-2017</p></span>
+  </div>
+
+  <div className={styles.tag9}>
+    <span><p>Primary School</p></span>
+  </div>
+
+  <div className={styles.tag10}>
+    <span><p>I was in three different schools. They were Koulumestarin koulu (2011-2014), Ymmerstan koulu (2014-2016) and Juvanpuiston koulu (2016-2017).</p></span>
+  </div>
+
+  <div className={styles.tag11}>
+    <span><p>2017-2020</p></span>
+  </div>
+
+  <div className={styles.tag12}>
+    <span><p>Junior High School</p></span>
+  </div>
+
+  <div className={styles.tag13}>
+    <span><p>I studied the whole junior high school time at Juvanpuiston koulu.</p></span>
+  </div>
+
+  <div className={styles.tag14}>
+    <span><p>Vocational College</p></span>
+  </div>
+
+  <div className={styles.tag15}>
+    <span><p>2020 -</p></span>
+  </div>
+
+  <div className={styles.tag16}>
+    <span><p>I study at Varia Myyrmäki.</p></span>
+  </div>
+
+  <div className={styles.tag17}>
+  <span><p></p></span>
+  </div>
+
+  <div className={styles.tag18}>
+  <span><p>Work Experience</p></span>
+  </div>
+
+  <div className={styles.tag19}>
+  <span><p>Kindergarten</p></span>
+  </div>
+
+  <div className={styles.tag20}>
+  <span><p>2018</p></span>
+  </div>
+
+  <div className={styles.tag21}>
+  <span><p>I worked at kindergarten when i was 14 years old.</p></span>
+  </div>
+
+  <div className={styles.tag22}>
+  <span><p>Food Restaurant</p></span>
+  </div>
+
+  <div className={styles.tag23}>
+  <span><p>2019</p></span>
+  </div>
+
+  <div className={styles.tag24}>
+  <span><p>I worked at restaurant when i was 15 years old.</p></span>
+  </div>
+
+  <div className={styles.tag25}>
+  <span><p></p></span>
+  </div>
+
+  <div className={styles.tag26}>
+  <span><p>My Hobbies</p></span>
+  </div>
+
+  <div className={styles.tag27}>
+  <span><p>I play video games and hang out with my friends.</p></span>
+  </div>
+
+  <div className={styles.tag28}>
+  <span><p></p></span>
+  </div>
+  
+</div>
     </div>
   )
 }
